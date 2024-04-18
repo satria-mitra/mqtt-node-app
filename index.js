feb93f53cc827f127e6a0f6c46293085f8d7fca4
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-18 04:10:14 +0100

    add firebase realtime database link

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('weathershare-57c93-firebase-adminsdk-aezjd-09356ce536.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://weathershare-57c93-default-rtdb.firebaseio.com", // replace "your-project-id" with your actual project ID
 });
 
 const db = admin.firestore();
