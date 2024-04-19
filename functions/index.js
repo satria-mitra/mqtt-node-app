f28d40f5dd2831ff056e0dc11d6af2fbbc7d0d38
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-19 19:48:22 +0100

    update function to save parameters

--- functions/index.js
+++ functions/index.js
@@ -32,8 +32,34 @@ async function setupMqttClient() {
     });
 
     mqttClient.on('message', (topic, message) => {
-        console.log(`Received message on ${topic}:`, message.toString());
+        try {
+            const msg = JSON.parse(message.toString());
+    
+            const deviceId = msg.data[0].vals[0];  // "Device ID"
+            const date = msg.data[0].vals[1].replace(/\//g, '-');  // "Date" formatted as YYYY-MM-DD
+            const time = msg.data[0].vals[2].replace(/:/g, '-');   // "Time" formatted as HH-mm
+            const taAvg = msg.data[0].vals[6];     // Temperature Average
+            const rhAvg = msg.data[0].vals[9];     // Relative Humidity Average
+    
+            // Log the values to be stored
+            console.log(`Storing data: Device ID - ${deviceId}, Date - ${date}, Time - ${time}, TA_AVG - ${taAvg}, RH_AVG - ${rhAvg}`);
+    
+            // Reference to the specific location in the Firebase Realtime Database
+            const ref = admin.database().ref(`devices/${deviceId}/${date}/${time}`);
+            ref.set({
+                temp_avg: taAvg,
+                rh_avg: rhAvg
+            }).then(() => {
+                console.log('Data stored successfully in Firebase RTDB');
+            }).catch((error) => {
+                console.error('Failed to store data in Firebase RTDB:', error);
+            });
+    
+        } catch (e) {
+            console.error('Error parsing message or extracting values:', e);
+        }
     });
+    
 
     mqttClient.on('error', (err) => {
         console.error('MQTT Connection error:', err);
